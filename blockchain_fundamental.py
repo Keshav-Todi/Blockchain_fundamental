c02edf6e2c2f0b7f3f87195c10300e5928159a15
Keshav-Todi <64375653+Keshav-Todi@users.noreply.github.com>
2024-11-09 14:34:12 +0530

    Add files via upload

---
+++ blockchain_fundamental.py
@@ -0,0 +1,58 @@
+import hashlib
+import time
+
+class Block:
+    def __init__(self, index, previous_hash, data, timestamp):
+        self.index = index
+        self.previous_hash = previous_hash
+        self.data = data
+        self.timestamp = timestamp
+        self.nonce = 0
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        block_string = f"{self.index}{self.previous_hash}{self.data}{self.timestamp}{self.nonce}"
+        return hashlib.sha256(block_string.encode()).hexdigest()
+
+    def mine_block(self, difficulty):
+        while self.hash[:difficulty] != "0" * difficulty:
+            self.nonce += 1
+            self.hash = self.calculate_hash()
+        print(f"Block mined: {self.hash}")
+
+class Blockchain:
+    def __init__(self):
+        self.chain = [self.create_genesis_block()]
+        self.difficulty = 4
+
+    def create_genesis_block(self):
+        return Block(0, "0", "Genesis Block", time.time())
+
+    def get_latest_block(self):
+        return self.chain[-1]
+
+    def add_block(self, data):
+        new_block = Block(len(self.chain), self.get_latest_block().hash, data, time.time())
+        new_block.mine_block(self.difficulty)
+        self.chain.append(new_block)
+
+    def is_chain_valid(self):
+        for i in range(1, len(self.chain)):
+            current_block = self.chain[i]
+            previous_block = self.chain[i - 1]
+            if current_block.hash != current_block.calculate_hash():
+                return False
+            if current_block.previous_hash != previous_block.hash:
+                return False
+        return True
+
+# Example usage
+my_blockchain = Blockchain()
+my_blockchain.add_block("First block data")
+my_blockchain.add_block("Second block data")
+
+print("Blockchain valid?", my_blockchain.is_chain_valid())
+
+# Printing the blockchain
+for block in my_blockchain.chain:
+    print(f"Index: {block.index}, Hash: {block.hash}, Previous Hash: {block.previous_hash}, Data: {block.data}, Nonce: {block.nonce}")
